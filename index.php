21ced47e24f1bd03fd25c0c8339277de7123b1d8
Rotem Haber <rotem.haber@gmail.com>
2016-09-23 23:36:37 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Password Generator</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<?php require 'logic.php'; ?>
+</head>
+<body>
+
+	<h1>Password Generator</h1>
+	<p class='password'></p>
+	<p>My first paragraph.</p>
+	<form action="index.php" method="GET">
+			<label for='numWords'># of Words</label>
+			<input maxlength=1 type='text' name='numWords' id='numWords'>  (Max 9)
+			<br>
+			<input type='checkbox' name='number' id='number' >
+			<label for='number'>Add a number</label>
+			<br>
+			<input type='checkbox' name='symbol' id='symbol' >
+			<label for='symbol'>Add a symbol</label>
+			<br>
+		<input type='submit' class='btn btn-default' value='Gimme Another'>
+		
+	</form>
+
+</body>
+</html>
---
+++ logic.php
@@ -0,0 +1,28 @@
+<?php
+
+#get the number of words:
+
+if (isset($_GET['numWords'])){
+	$numWords;
+	$numWords = $_GET['numWords'];
+}
+else{
+	$numWords = 4;
+}
+
+#define the words array
+$words = array("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten");
+$rand_keys = array_rand($words, $numWords);
+$password = "";
+for($i=0; $i<$numWords; $i++){
+	$password.= $words[$rand_keys[$i]];
+}
+
+echo $password;
+
+if (isset($_GET['numWords'])){
+	$numWords;
+	$numWords = $_GET['numWords'];
+}
+
+
